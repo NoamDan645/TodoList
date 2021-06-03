3bae5b293e5b06be8f76428732e021d985eb1a1c
NoamDan645 <78931414+NoamDan645@users.noreply.github.com>
2021-06-03 10:51:35 +0300

    Add files via upload

---
+++ TodoList.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("add");
+const doList = document.getElementById("");
+
+form.addEventListener("submit", addDo); // ERROR
+doList.addEventListener("click", removeDo); //ERROR
+
+function addDo() {
+  var newDo = document.getElementById("thing").value;
+  var li = document.createElement("li");
+  li.className = "list-group-item";
+  li.appendChild(document.createTextNode(newDo));
+  doList.appendChild(li);
+
+  var delButton = document.createElement("button");
+  delButton.className = "btn btn-danger btn-sm float-right delete";
+  delButton.appendChild(document.createTextNode("delete"));
+  li.appendChild(delButton);
+  doList.appendChild(li);
+}
+
+function removeDo(e) {
+  if (e.target.classList.contains("delete")) {
+    var li = e.target.parentElement;
+    doList.removeChild(li);
+  }
+}
